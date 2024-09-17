4933675ec0c672e92124eef8c13c413b8bfa755a
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-09-17 13:46:03 +0400

    Change to StandardJS style

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.on('window-all-closed', function() {
 })
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 1360, height: 800})
     mainWindow.loadURL('file://' + __dirname + '/public/index.html')
     mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function() {
--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
 
 const App = (props) => {
   return <div>Test!</div>;
